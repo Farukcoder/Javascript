6c45ed8b37861ef071db9554f4b613bed04c6520
Farukcoder <akomarfci@gmail.com>
2021-04-20 16:09:32 +0600

    initial commit

---
+++ class1 variable/app.js
@@ -0,0 +1,27 @@
+// alert("hellow");
+
+// var, let , const
+
+// var functon_scoped
+
+// var name = "hellow world";
+// console.log(name);
+
+// let global_scoped
+
+// let age = 40;
+// console.log(age);
+
+// example
+// let input = prompt("whats your name?");
+// alert("hello".input);
+
+// let address = "dhaka";
+// address= "dhaka";
+// console.log(address);
+
+// const
+const city = ["talha","nasir","40","50"]
+console.log(city);
+
+
---
+++ class2 datatypes/app.js
@@ -0,0 +1,50 @@
+//datatype javascript
+
+//primitive datatype 
+    // boolean
+    // Number
+    // string 
+    // Null 
+    // Symbol
+    // undefined 
+
+//complex datatype
+    // object 
+
+// boolean 
+
+    // let value = true;
+    // console.log(typeof value);
+
+// Number
+let num = 45;
+console.log(typeof num);
+
+//String
+let str = "hello world";
+console.log(typeof str);
+
+//Null
+let nul = null;
+console.log(typeof null);
+
+//symbole
+let sym = Symbol();
+console.log(typeof sym);
+
+//undefine
+let und = undefined;
+console.log(typeof und);
+
+//complex datatype 
+//object
+
+var obj = {
+    name:"omar",
+    roll:45,
+    class:10
+}
+console.log(typeof obj);
+
+var ob=['hi','hello']; 
+console.log(typeof ob);
